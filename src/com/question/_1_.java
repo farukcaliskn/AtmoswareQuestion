93466daf3b8e65aee54400d223fc187a009299b0
farukcaliskn <faruk.caliskn@outlook.com>
2022-06-26 19:26:42 +0300

    added question

---
+++ src/com/question/_1_.java
@@ -0,0 +1,115 @@
+package com.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _1_ {
+	public static void main(String[] args) {
+		dataInput();
+	}
+
+	// Alfabenin ve kelimelerin belirlenmesi control metodunun çağırılması
+	public static void dataInput() {
+
+		// Kontrol edilmesinin kolaylığı açısından veri girişini manuel olarak
+		// bıraıktım.
+
+		String str = "iabcçdefgğhıjklmnoöprsştuvyz";
+		List<String> words = new ArrayList<>();
+		words.add("İsmet");
+		words.add("abdulsemet");
+		words.add("aksu");
+		words.add("aksi");
+		words.add("karadeniz");
+		words.add("onurcan");
+		words.add("pehlil");
+		words.add("sargın");
+		words.add("şemsettin");
+
+		control(str, words.stream().map(String::toLowerCase).collect(Collectors.toList()));
+	}
+
+	// Oluşturulan clone üzerinde yapılan işlemlerden sonra dizinin ilk hali ile
+	// kontrol edilmesi.
+	public static boolean control(String str, List<String> words) {
+		List<Character> alphabet = new ArrayList<>();
+		alphabet(str, alphabet);
+		List<String> cloned_list = new ArrayList<String>(words);
+		returSortedList(alphabet, cloned_list);
+		System.out.println(words);
+		System.out.println(cloned_list);
+		boolean control = false;
+		if (words.equals(cloned_list)) {
+			System.out.println("İletilen Kelimeler sıralıdır");
+			control = true;
+		} else {
+			System.out.println("İletilen kelimeler sıralı değildir.");
+		}
+		return control;
+	}
+
+	// String olarak girilen alfabe listeye eleman olarak ekleniyor.
+	public static List<Character> alphabet(String str, List<Character> alphabet) {
+
+		for (char ch : str.toCharArray()) {
+			alphabet.add(ch);
+		}
+		return alphabet;
+
+	}
+
+	// Kelimelerin Sıralanması
+	public static List<String> returSortedList(List<Character> alphabet, List<String> words) {
+
+		for (int i = 0; i < words.size(); i++) {
+			for (int j = i; j < words.size(); j++) {
+				if (alphabet.indexOf(words.get(i).charAt(0)) < alphabet.indexOf(words.get(j).charAt(0))) {
+
+				}
+
+				else if (alphabet.indexOf(words.get(i).charAt(0)) > alphabet.indexOf(words.get(j).charAt(0))) {
+					String deger = words.get(i);
+					words.set(i, words.get(j));
+					words.set(j, deger);
+				} else {
+					// sortListWordsControl o durumdaki i ve j konumu hangi kelimeye bakıldığını
+					// anlamak için gönderiliyor ve fonksiyon hangisinin öncelikli olduğunu kontrol
+					// ediyor
+					sortListWordsControl(alphabet, words, i, j);
+				}
+			}
+
+		}
+
+		return words;
+	}
+
+	// Kelimeler sıralanırken aynı harf düzeninde olan kelimelerden hangisinin önce
+	// geleceğine karar vermek için returSortedList metodunda çağırılan fonksiyon
+	public static List<String> sortListWordsControl(List<Character> alphabet, List<String> words, int i, int j) {
+
+		String firstWord = "";
+		int loopSize = Math.max(words.get(i).length(), words.get(j).length());
+		for (int k = 0; k < loopSize; k++) {
+			if (alphabet.indexOf(words.get(i).charAt(k)) > alphabet.indexOf(words.get(j).charAt(k))) {
+				firstWord = words.get(j);
+				break;
+			} else if (alphabet.indexOf(words.get(i).charAt(k)) < alphabet.indexOf(words.get(j).charAt(k))) {
+				firstWord = words.get(i);
+				break;
+			} else {
+			}
+		}
+		if (words.get(i).equals(firstWord)) {
+
+		} else {
+			String temp = words.get(i);
+			words.set(i, words.get(j));
+			words.set(j, temp);
+		}
+
+		return words;
+
+	}
+}
---
+++ src/com/question/_2_.java
@@ -0,0 +1,99 @@
+package com.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _2_ {
+	public static void main(String[] args) {
+		dataInput();
+	}
+
+	// Alfabenin ve kelimelerin belirlenmesi control metodunun çağırılması
+	public static void dataInput() {
+
+		// Kontrol edilmesinin kolaylığı açısından veri girişini manuel olarak
+		// bıraıktım.
+
+		String str = "iabcçdefgğhıjklmnoöprsştuvyz";
+		List<String> words = new ArrayList<>();
+		words.add("İsmet");
+		words.add("abdulsemet");
+		words.add("aksi");
+		words.add("aksu");
+		words.add("karadeniz");
+		words.add("onurcan");
+		words.add("pehlil");
+		words.add("sargın");
+		words.add("şemsettin");
+
+		// words.add("irfan");
+
+		System.out.println();
+		Boolean result = control(str, words.stream().map(String::toLowerCase).collect(Collectors.toList()));
+		String sorted = (result == true ? "Liste sıralıdır" : "Liste sıralı değildir");
+		System.out.println(sorted);
+	}
+
+	// Kontrol için gerekli fonksiyonlara parametrelerin geçilmsei ve sonucun
+	// döndürülmesi
+	public static boolean control(String str, List<String> words) {
+		List<Character> alphabet = new ArrayList<>();
+		alphabet(str, alphabet);
+
+		return returnSortedListControl(alphabet, words);
+	}
+
+	// String olarak girilen alfabe listeye eleman olarak ekleniyor.
+	public static List<Character> alphabet(String str, List<Character> alphabet) {
+
+		for (char ch : str.toCharArray()) {
+			alphabet.add(ch);
+		}
+		return alphabet;
+
+	}
+
+	/*
+	 * Kelimelerin öncelik sırasının kontrol edilip ilk hatada false olarak
+	 * döndürülmesi ve harf sıralaması aynı ilerleyen kelimelerde kontrol için
+	 * kelime önceliğine bakılacak fonksiyonun çağrılmasından sonra false dönülmesi
+	 */
+	public static boolean returnSortedListControl(List<Character> alphabet, List<String> words) {
+
+		for (int i = 0; i < words.size(); i++) {
+			for (int j = i; j < words.size(); j++) {
+				if (alphabet.indexOf(words.get(i).charAt(0)) < alphabet.indexOf(words.get(j).charAt(0))) {
+
+				} else if (alphabet.indexOf(words.get(i).charAt(0)) > alphabet.indexOf(words.get(j).charAt(0))) {
+					return false;
+				} else {
+					boolean result = WordsControl(alphabet, words.get(i), words.get(j));
+					if (result == false) {
+						return false;
+					}
+				}
+			}
+
+		}
+
+		return true;
+	}
+
+	/*
+	 * Kelimeler sıralanırken aynı harf düzeninde olan kelimelerden hangisinin önce
+	 * geleceğine karar vermek için returSortedList metodunda çağırılan fonksiyon
+	 */
+	public static boolean WordsControl(List<Character> alphabet, String w1, String w2) {
+		int loopSize = Math.max(w1.length(), w2.length());
+		for (int k = 0; k < loopSize; k++) {
+			if (alphabet.indexOf(w1.charAt(k)) > alphabet.indexOf(w2.charAt(k))) {
+				return false;
+			} else if (alphabet.indexOf(w1.charAt(k)) < alphabet.indexOf(w2.charAt(k))) {
+				return true;
+			}
+		}
+		return true;
+
+	}
+}
